53bad2f6e82fbfa9f742d9f4cbd8230b0f805e70
Philip Huysmans <philip.huysmans@gmail.com>
2015-05-23 12:21:03 +0200

    code reorganization

--- test.js
+++ test.js
@@ -1,70 +1,7 @@
 // MOPIDY =======================================================================================
+// ==============================================================================================
 
-// Initaite mopidy
-var Mopidy = require("mopidy");
-var mopidy = new Mopidy({
-  webSocketUrl: "ws://localhost:6680/mopidy/ws",
-  callingConvention: "by-position-only"
-});
-
-var getTrackDescription = function(track) {
-  return track.name + " by " + track.artists[0].name + " from " + track.album.name + ". URI: " + track.uri;
-};
-
-var printPlaylists = function() {
-  mopidy.playlists.getPlaylists().then(function(playlists) {
-    for(var i = 0; i < playlists.length; i++) {
-      console.log(playlists[i].name);
-    }
-  })
-  .catch(console.error.bind(console))
-  .done();
-};
-
-var notifySong = function() {
-  return mopidy.playback.getCurrentTrack().then(function(track) {
-    console.log("Now playing: ", getTrackDescription(track));
-  });
-};
-
-var loadPlaylist = function() {
-  //printPlaylists();
-  mopidy.tracklist.getLength().then(function(len){
-    console.log(len);
-  });
-  /*
-  mopidy.playlists.getPlaylists().then(function(playlists){
-    var playlist = playlists[0];
-    console.log("Loading playlist: ", playlist.name);
-    return mopidy.tracklist.add(playlist.tracks).then(function(tlTracks) {
-      return mopidy.playback.play(tlTracks[0]).then(notifySong);
-    }); 
-  })
-  .catch(console.error.bind(console))
-  .done();
-*/
-};
-
-var startPlaying = function() {
-  return mopidy.playback.play().then(function() {
-    return notifySong(); 	
-  });
-};
-
-var skipTrack = function() {
-  return mopidy.playback.next().then(function(){
-    return notifySong();
-  });
-};
-
-var listTracks = function() {
-  mopidy.library.lookup("podcast+http://www.npr.org/rss/podcast.php?id=510019").then(function(lookedUpTracks) {
-    console.log(lookedUpTracks[0].uri, "name: " + lookedUpTracks[0].name);
-    
-  });
-
-};
-
+// OPERATIONAL FUNCTIONALITY ======================================================================
 var init = function() {
   bindButtons();
 
@@ -89,14 +26,84 @@ var init = function() {
   .done();
 };
 
+var loadPlaylist = function() {
+  //printPlaylists();
+  mopidy.tracklist.getLength().then(function(len){
+    console.log(len);
+  });
+  /*
+  mopidy.playlists.getPlaylists().then(function(playlists){
+    var playlist = playlists[0];
+    console.log("Loading playlist: ", playlist.name);
+    return mopidy.tracklist.add(playlist.tracks).then(function(tlTracks) {
+      return mopidy.playback.play(tlTracks[0]).then(notifySong);
+    }); 
+  })
+  .catch(console.error.bind(console))
+  .done();
+*/
+};
 
-// GPIO =======================================================================================
+var notifyPlaylists = function() {
+  mopidy.playlists.getPlaylists().then(function(playlists) {
+    console.log("These are the playlists: ");
+    for(var i = 0; i < playlists.length; i++) {
+      console.log("Playlist name: " + playlists[i].name);
+    }
+  })
+  .catch(console.error.bind(console))
+  .done();
+};
+
+var notifySong = function() {
+  return mopidy.playback.getCurrentTrack().then(function(track) {
+    console.log("Now playing: ", getTrackDescription(track));
+  });
+};
+
+var startPlaying = function() {
+  return mopidy.playback.play().then(function() {
+    return notifySong();  
+  });
+};
+
+var skipTrack = function() {
+  return mopidy.playback.next().then(function(){
+    return notifySong();
+  });
+};
+
+// GETTER FUNCTIONALITY ===========================================================================
+var getTrackDescription = function(track) {
+  return track.name + " by " + track.artists[0].name + " from " + track.album.name + ". URI: " + track.uri;
+};
+
+// TEST FUNCTIONALITY =============================================================================
+var listTracks = function() {
+  mopidy.library.lookup("podcast+http://www.npr.org/rss/podcast.php?id=510019").then(function(lookedUpTracks) {
+    console.log(lookedUpTracks[0].uri, "name: " + lookedUpTracks[0].name);    
+  });
+};
+
+// This should return multiple podcasts
+var listPodcasts = function() {
+  mopidy.library.lookup("podcast+https://gpodder.net/user/Sharp6/favorites.xml").then(function(lookedUpTracks) {
+    console.log(lookedUpTracks.length);
+  });
+};
+
+
+
+
+
+// GPIO =========================================================================================
+// ==============================================================================================
 // button is attaced to pin 17, led to 18
 var GPIO    = require('onoff').Gpio;
 //var led     = new GPIO(18, 'out');
-var button1  = new GPIO(2, 'in', 'both');
-var button2  = new GPIO(3, 'in', 'both');
-var button3  = new GPIO(4, 'in', 'both');
+var button1  = new GPIO(2, 'in', 'rising');
+var button2  = new GPIO(3, 'in', 'rising');
+var button3  = new GPIO(4, 'in', 'rising');
 
 
 // CALLBACKS =======================================================================================
@@ -138,9 +145,16 @@ function f3(err, state) {
 
 function bindButtons() {
   button1.watch(listTracks);
-  button2.watch(f2);
+  button2.watch(listPodcasts);
   button3.watch(f3);
 }
 
-// START HERE =======================================================================================
+//  APP START HERE =======================================================================================
+// =======================================================================================================
+// Initiate mopidy
+var Mopidy = require("mopidy");
+var mopidy = new Mopidy({
+  webSocketUrl: "ws://localhost:6680/mopidy/ws",
+  callingConvention: "by-position-only"
+});
 mopidy.on("state:online", init);
