4dae06d356eac9f2b02eee2e4e91e05fb3d75b14
Philip Huysmans <philip.huysmans@gmail.com>
2015-05-28 19:03:45 +0000

    errors

--- app.js
+++ app.js
@@ -8,15 +8,12 @@ var rprMopidy = require("./rprMopidy");
 
 
 var GPIO    = require('onoff').Gpio;
-//var led     = new GPIO(18, 'out');
 var button1  = new GPIO(2, 'in', 'rising');
 var button2  = new GPIO(3, 'in', 'rising');
 var button3  = new GPIO(4, 'in', 'rising');
 
-
 // CALLBACKS =======================================================================================
-function bindButtons() {
-  button1.watch(volumeUp);
-  button2.watch(volumeDown);
-  button3.watch(switchMode);
-}
+button1.watch(rprMopidy.volumeUp);
+button2.watch(rprMopidy.volumeDown);
+button3.watch(rprMopidy.switchMode);
+
