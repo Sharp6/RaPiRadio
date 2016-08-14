e336f3ff0959d52253a6e583a0566dc99fe18ca3
Philip Huysmans <philip.huysmans@gmail.com>
2016-08-14 11:47:23 +0200

    added sleeper module

---
+++ sleeper.js
@@ -0,0 +1,15 @@
+module.exports = (function() {
+	var sleeper;
+
+	function enableSleepMode() {
+		if(sleeper) {
+			clearTimeout(sleeper);
+		}
+		sleeper = setTimeout(goToSleep, 5000);
+		console.log("Sleepmode activated");
+	}
+
+	function goToSleep() {
+		console.log("Nighty night!");
+	}
+})();
