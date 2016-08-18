82e34700abee53f40226520b3cd42a4f452ff332
Philip Huysmans <philip.huysmans@gmail.com>
2016-08-18 13:04:35 +0200

    test

--- onoffControls.js
+++ onoffControls.js
@@ -3,7 +3,7 @@ var onoffControls = function() {
 	var Gpio = require('onoff').Gpio;
 	//button = new Gpio(20, 'in', 'rising', {debounceTimeout: 300});
 	var buttons = new Array();
-	var pins = [];
+	var pins = [20,16,7,8,25,24];
 
 	function init() {
 		return Promise.resolve()
